f55720a8c07986db5bea13746f776055580280a0
hejing <jing.he@itechchoice.com>
2026-01-23 18:05:19 +0800

    feat: 瘦身

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,9 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        docs: false, // 禁用文档功能
         blog: {
+          routeBasePath: "/", // 博客作为首页
           showReadingTime: true,
           feedOptions: {
             type: ["rss", "atom"],
@@ -90,13 +85,6 @@ const config = {
         },
         items: [
           {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Tutorial",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
             href: "https://github.com/itechchoice",
             label: "GitHub",
             position: "right",
@@ -107,28 +95,11 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
+            title: "Blog",
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "X",
-                href: "https://x.com/docusaurus",
+                label: "All Posts",
+                to: "/",
               },
             ],
           },
@@ -136,17 +107,13 @@ const config = {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/itechchoice",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Die Yi. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
